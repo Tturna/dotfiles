35c96d4559614da261eac7fd153ddc3c27469c55
Tturna <eemelijuponaho@gmail.com>
2024-12-05 13:14:53 +0200

    better laptop polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/zsh
 
 polybar-msg cmd quit
-polybar main & disown
 
 hostnamectl chassis | grep -q "laptop" > /dev/null
 
-# if not laptop, start bars on all desktop displays
 if [[ $? != 0 ]] then
+    # if not laptop, start bars on all desktop displays
     polybar left & disown
     polybar right & disown
+else
+    # if is laptop, define custom modules
+    export POLYBAR_MODULES_RIGHT="cpu battery pulseaudio xkeyboard wlan date"
 fi
+
+polybar main & disown
